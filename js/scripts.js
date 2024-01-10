24c319fe00054114f087f0aaf94576a0bdc80f44
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-01-10 20:06:10 +0100

    esercizio finito

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            counterSlide : 0,
+            slides : [
+                    {
+                        image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morale',
+                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    }, {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    }, {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    }, {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    }, {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    }
+            ], 
+
+        };
+    },
+    methods: {
+        next(){
+            if(this.counterSlide < this.slides.length - 1){
+                
+                this.counterSlide ++;
+        
+            }else{
+                this.counterSlide = 0;
+            }
+
+            
+        },
+
+        prev(){
+        
+            if((this.counterSlide !== this.slides.length) && (this.counterSlide !== 0)){
+
+                this.counterSlide -= 1 ;
+
+            }else{
+                this.counterSlide = this.slides.length - 1 ;
+            }
+            
+        }
+    }
+}).mount('#app');
